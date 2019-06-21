1ac9244e89668bd6f355c363fa81065a00788155
Rinzlo <joshuarco@gmail.com>
2019-06-20 22:00:04 -0700

    hoisting

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-var a = 'Hello World'
+// Hoisting Sets up memory space 
+// for variables and functions.
+// Functions are put into memory
+// in their entirity and variables are
+// set to undefined.
+
+b();
+console.log(a);
+
+var a = 'Hello World';
 
 function b() {
-    
+    console.log('Called b!');
 }
