0011f3f5aead0ed61fb3847571ba957d1d51a156
Rinzlo <joshuarco@gmail.com>
2019-06-26 15:06:46 -0700

    execution context

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-var a = 'hellow world';
-console.log(a);
+function b() {
+    var myVar;
+    console.log(myVar);
+}
 
-if (a === undefined)
-    console.log('a is undefined!');
-else
-    console.log('a is defined!');
+function a() {
+    var myVar = 2;
+    console.log(myVar);
+    b();
+}
+
+var myVar = 1;
+console.log(myVar);
+a();
